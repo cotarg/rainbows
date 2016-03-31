8a34a75939a02334c94498a8d04e6ba4a0c4a5f8
Rowan Cota <cota.rg@gmail.com>
2016-03-31 10:29:27 -0700

    made change to rainbow variable per feedback

--- rainbows.rb
+++ rainbows.rb
@@ -19,13 +19,15 @@ color_lookup = {
   [255,0,125] => "raspberry"
 }
 
-rainbow = [{:red => {r: 255, g: 0, b: 0}}, 
+rainbow = [
+          {:red => {r: 255, g: 0, b: 0}}, 
           {:orange => {r: 255, g: 125, b: 0}}, 
           {:yellow => {r: 255, g: 255, b: 0}}, 
           {:green => {r: 0, g: 255, b: 0}}, 
           {:blue => {r: 0, g: 0, b: 255}}, 
           {:indigo => {r: 75, g: 0, b: 130}}, 
-          {:violet => {r: 125, g: 0, b: 255}}]
+          {:violet => {r: 125, g: 0, b: 255}}
+          ]
 
 def mix_colors(color1,color2)
   mixed_color = []
