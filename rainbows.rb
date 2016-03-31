f24b31310a15349a1065123731a9c93133126bea
Rowan Cota <cota.rg@gmail.com>
2016-03-30 21:37:25 -0700

    commiting so I can sub pull request

--- rainbows.rb
+++ rainbows.rb
@@ -17,6 +17,10 @@ color_lookup = {
   [255,0,125] => "raspberry"
 }
 
+rainbow = [{:red => {r: 255, g: 0, b: 0}}, {:orange => {r: 255, g: 125, b: 0}}, 
+  {:yellow => {r: 255, g: 255, b: 0}}, {:green => {r: 0, g: 255, b: 0}}, {:blue => {r: 0, g: 0, b: 255}}, 
+  {:indigo => {r: 75, g: 0, b: 130}}, {:violet => {r: 125, g: 0, b: 255}}]
+
 def mix_colors(color1,color2)
   mixed_color = []
   3.times do |i|
@@ -31,9 +35,7 @@ puts "#{color_lookup[red]} #{red} + #{color_lookup[green]} #{green} = #{color_lo
 # Let's create a rainbow of colors as an array of hashes, where each hash contains the name of the color (key) 
 # and labeled RGB values (value).
 
-rainbow = [{:red => {r: 255, g: 0, b: 0}}, {:orange => {r: 255, g: 125, b: 0}}, 
-  {:yellow => {r: 255, g: 255, b: 0}}, {:green => {r: 0, g: 255, b: 0}}, {:blue => {r: 0, g: 0, b: 255}}, 
-  {:indigo => {r: 75, g: 0, b: 130}}, {:violet => {r: 125, g: 0, b: 255}}]
+
 
 new_color = mix_colors(red,green)
 puts "#{color_lookup[red]} #{red} + #{color_lookup[green]} #{green} = #{color_lookup[new_color]} #{new_color}"
